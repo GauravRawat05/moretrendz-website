81a3cfdae2c21121af39d586c6bb010a36d9832d
Gaurav Singh Rawat <itsgauravrawat2005@gmail.com>
2025-07-29 09:58:24 +0530

    fixed product page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     "./*.html", // Scans all HTML files in the root
     "./public/**/*.html", // Scans all HTML files inside the public folder
+    "./js/**/*.js", // <-- ADD THIS LINE
   ],
   theme: {
     extend: {},
